d5cdfec7933fa7648fce7d20313ea7c5f24a4eb5
mithun-2025 <ss.mithunkumar00@gmail.com>
2025-10-20 15:02:16 +0600

    Initial Commit

---
+++ MVVM-IOS-Project-Swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MVVM-IOS-Project-Swift
+//
+//  Created by Second Source on 10/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MVVM-IOS-Project-Swift/MVVM_IOS_Project_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_IOS_Project_SwiftApp.swift
+//  MVVM-IOS-Project-Swift
+//
+//  Created by Second Source on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_IOS_Project_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
